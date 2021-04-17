9404390d3c8fbad3ae814c752bf98bfee49ff4e7
Edmilson <edmilson@univision.net.br>
2021-04-17 10:43:11 -0300

    add comentario api list todos os alunos
    add comentario api list todos os alunos

--- demo/src/main/java/com/sistema/demo/controller/AlunoController.java
+++ demo/src/main/java/com/sistema/demo/controller/AlunoController.java
@@ -23,6 +23,11 @@ public class AlunoController {
 	@Autowired
 	private AlunoService alunoService;
 
+	/**
+	 * Consultar todos os alunos
+	 * 
+	 * @return
+	 */
 	@GetMapping
 	public ResponseEntity<?> findAll() {
 
